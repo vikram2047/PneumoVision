571e74f81cfe17b5cb058769dcb8fdc067d28d83
vikram2047 <vikramvarikuti@gmail.com>
2026-07-05 15:53:59 +0530

    feat: add CNN model with pneumonia classification logic

---
+++ Model.py
@@ -0,0 +1,27 @@
+from tensorflow.keras import layers, models
+import numpy as np
+import cv2
+
+
+class MyModel:
+    def __init__(self):
+        self.IMAGE_SIZE = 148
+        self.model = models.Sequential()
+        self.model.add(layers.Conv2D(32, (3, 3), input_shape=(self.IMAGE_SIZE,self.IMAGE_SIZE,1), activation='relu'))
+        self.model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+        self.model.add(layers.BatchNormalization())
+        self.model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+        self.model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+        self.model.add(layers.Flatten())
+        self.model.add(layers.Dense(1, activation='sigmoid'))
+        self.model.compile(loss="binary_crossentropy", optimizer='adam', metrics=["accuracy"])
+        self.model.load_weights('./static/weights.h5')
+
+    def predict(self):
+        i = cv2.imread('./static/image.png', cv2.IMREAD_GRAYSCALE)
+        i = cv2.resize(i, (self.IMAGE_SIZE, self.IMAGE_SIZE))
+        i = np.array(i).reshape(-1, self.IMAGE_SIZE, self.IMAGE_SIZE, 1)
+        i = np.array(i).astype('float')/255.0
+        prediction = self.model.predict([i])
+        return ((['NORMAL', 'PNEUMONIA'][int(prediction.round())], prediction[0][0]))
+
